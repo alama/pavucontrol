3775d948dff929d495083975bab0072d4e2ca487
Alam Arias <Alam.GBC@gmail.com>
2011-09-27 11:14:24 -0400

    need automake 1.11 in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pavucontrol. If not, see <http://www.gnu.org/licenses/>.
 
-VERSION=1.9
+VERSION=1.11
 
 run_versioned() {
     local P
